52910d0796b06de1086484c4598fb263ecdbb1a4
Kuljot Biring <ksbiring1@gmail.com>
2021-04-11 08:51:26 -0700

    added credentials file

---
+++ db_credentials.py
@@ -0,0 +1,14 @@
+"""
+author:         Sandro Aguilar
+module name:    db_credentials
+purpose:        this module holds my credentials required to access the
+                MySQL database in my local machine
+"""
+
+#-------------------------------------------------------------------------
+# Description:              holds DB credentials
+#-------------------------------------------------------------------------
+host = 'localhost'
+user = 'root'
+password = 'a1s2d3F$G%H^'
+db = 'cs340_project'
